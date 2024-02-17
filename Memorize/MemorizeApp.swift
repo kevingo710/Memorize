8bcb60de0535171114c6e84ff068a0fa8a6fafcc
Kevin Gracia <kevingracia07@gmail.com>
2024-02-17 09:19:14 +0100

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Kevin Gracia on 17/02/24.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
