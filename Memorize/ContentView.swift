32c2f8be3763f4096dbcca1efd6a71a3929d0312
Kevin Gracia <kevingracia07@gmail.com>
2024-02-19 13:34:41 +0100

    feat: add @State isFaceUp & toggle on CardView

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -9,13 +9,35 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
+        HStack(content: {
+            CardView(isFaceUp: true)
+            CardView(isFaceUp: true)
+            CardView(isFaceUp: false)
+            CardView(isFaceUp: true)
+        })
+      
+        .foregroundColor(.orange)
         .padding()
+    }
+}
+
+struct CardView: View {
+    @State var isFaceUp: Bool = false
+    var body: some View {
+        ZStack {
+            let base = Circle()
+            if isFaceUp {
+               base.fill(.white)
+               base.strokeBorder(lineWidth: /*@START_MENU_TOKEN@*/1.0/*@END_MENU_TOKEN@*/)
+                Text("🏂").font(.largeTitle)
+            } else {
+                base.fill()
+            }
+        }
+        .onTapGesture {
+            //isFaceUp = !isFaceUp
+            isFaceUp.toggle()
+        }
     }
 }
 
